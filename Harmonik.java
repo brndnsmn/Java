3e4a95ecb95f29de05e2098a6b8f68d25fca39b8
brndnsmn <104435132+brndnsmn@users.noreply.github.com>
2022-12-23 00:14:57 +0300

    HarmonicDongu

--- Harmonik.java
+++ Harmonik.java
@@ -1,15 +1,1 @@
 
-public class Harmonik {
-
-	public static void main(String[] args) {
-		int[] list = {1,2,3,4,5,6,7,8,9,10};
-		double sum = 0;
-		
-		for (int i = 0; i<list.length; i++) {
-			sum = (sum + (1/list[i]));
-		}
-		double harmonic = list.length/sum; 
-		System.out.println(harmonic);
-
-	}
-}
